1d2c0ec0ad3423b3b364a305736252165f69e320
wooopers <kngordon@msn.com>
2023-08-24 19:24:11 -0500

    changest to script.js and all htmls to add next button and show questions

---
+++ assets/index.html/assets/js/script.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.querySelector('.start-button');
+  const instructionsSection = document.querySelector('#instructions');
+  const questionsSection = document.querySelector('.multiple-choice-questions');
+  const questionContainer = document.querySelector('#multiple-choice-questions');
+  const winMessage = document.querySelector('.win');
+  const loseMessage = document.querySelector('.lose');
+  const resetButton = document.querySelector('.reset-button');
+  const timerCount = document.querySelector('.timer-count');
+  const timerSection = document.querySelector('.timer');
+
+  const questionPaths = [
+    'question1.html', // Path to your question HTML files
+    'question2.html',
+    'question3.html',
+    // Add more paths as needed
+  ];
+  let currentQuestion = 0;
+
+  function loadQuestion() {
+    if (currentQuestion < questionPaths.length) {
+      fetch(questionPaths[currentQuestion])
+        .then(response => response.text())
+        .then(data => {
+          questionContainer.innerHTML = data;
+          bindQuestionEventListeners();
+        });
+    } else {
+      endGame();
+    }
+  }
+
+  function nextQuestion() {
+    currentQuestion++;
+    loadQuestion();
+  }
+
+  function startQuiz() {
+    startButton.style.display = 'none';
+    instructionsSection.style.display = 'none';
+    questionsSection.classList.remove('hidden');
+    nextQuestion();
+  }
+
+  function checkAnswer(selectedChoice, correctAnswer) {
+    if (selectedChoice === correctAnswer) {
+      winMessage.style.display = 'block';
+    } else {
+      loseMessage.style.display = 'block';
+    }
+
+    setTimeout(() => {
+      winMessage.style.display = 'none';
+      loseMessage.style.display = 'none';
+      nextQuestion();
+    }, 1000);
+  }
+
+  function endGame() {
+    questionsSection.style.display = 'none';
+    resetButton.style.display = 'block';
+  }
+
+  function bindQuestionEventListeners() {
+    const choices = document.querySelectorAll('.choice');
+    choices.forEach(choice => {
+      choice.addEventListener('click', () => {
+        const selectedChoice = choice.textContent.trim();
+        const currentQuestionData = questions[currentQuestion];
+        checkAnswer(selectedChoice, currentQuestionData.answer);
+      });
+    });
+  }
+
+  startButton.addEventListener('click', startQuiz);
+  resetButton.addEventListener('click', () => {
+    resetButton.style.display = 'none';
+    startButton.style.display = 'block';
+    instructionsSection.style.display = 'block';
+    currentQuestion = 0; // Reset currentQuestion to 0
+  });
+
+});
+
